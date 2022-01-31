497aeacbf89021b9ba598afb951d3399d67e8a99
hyooosong <gythd1998@gmail.com>
2022-01-31 20:35:48 +0900

    [refactor] Refactoring by Code Review

--- app/src/main/java/com/example/moviereview/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/example/moviereview/ui/search/SearchViewModel.kt
@@ -14,20 +14,34 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SearchViewModel @Inject constructor(private val dataSource: MovieDataSource) : ViewModel() {
+
+    enum class Event {
+        NOT_INPUT
+    }
+
     var editTextSearch = MutableLiveData<String>()
 
     private val _movieList = MutableLiveData<List<MovieResponse.Item>>()
     val movieList: LiveData<List<MovieResponse.Item>> = _movieList
 
+    private val _event = MutableLiveData<Event>()
+    val event : LiveData<Event> = _event
+
     fun getMovieList() {
         viewModelScope.launch(Dispatchers.IO) {
-            val list = dataSource.getMovieList(
-                "CqffDKypVaajB6acrJLK",
-                "LJ0prk1zCh",
-                editTextSearch.value ?: " "
-            )
-            withContext(Dispatchers.Main) {
-                _movieList.value = list.items
+            editTextSearch.value?.let {
+                val list = dataSource.getMovieList(
+                    "CqffDKypVaajB6acrJLK",
+                    "LJ0prk1zCh",
+                    editTextSearch.value ?: " "
+                )
+                withContext(Dispatchers.Main) {
+                    _movieList.value = list.items
+                }
+            } ?: run {
+                withContext(Dispatchers.Main) {
+                    _event.value = Event.NOT_INPUT
+                }
             }
         }
     }
